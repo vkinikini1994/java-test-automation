308522877195ccbfa40a55a548cfb8ca2de5ac12
vkinikini1994 <46568720+vkinikini1994@users.noreply.github.com>
2020-04-28 16:19:50 +0530

    initial commit

---
+++ src/main/java/sonar/App.java
@@ -0,0 +1,17 @@
+package sonar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String a ="";
+    	System.out.println( "Hello World!" );
+    	String c="e";
+    	String d="d";
+    	String g="d";
+    }
+}
